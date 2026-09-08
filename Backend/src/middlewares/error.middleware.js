f9ce6f92b283f7215fb948fb82f800e9324f05e7
MOHD TALHA <talhamd0110@gmail.com>
2026-09-08 13:54:30 +0530

    feat: add error handling middleware for centralized error management

---
+++ Backend/src/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+
+    return res.status(statusCode).json({
+        success: false,
+        message: err.message || "Something went wrong",
+        errors: err.errors || [],
+    });
+};
+
+export { errorHandler };
