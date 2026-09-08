acd885a2ffdf13a4cdfa1de333995d6bf9c32ca2
MOHD TALHA <talhamd0110@gmail.com>
2026-09-08 13:54:54 +0530

    feat: implement user routes for registration, login, logout, and current user retrieval

---
+++ Backend/src/routes/user.route.js
@@ -0,0 +1,18 @@
+import {Router} from "express"
+import { getCurrentUser, loginUser, logout, registerUser } from "../controller/user.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,logout)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+// router.route("/logout").post(verifyJWT, logoutUser);
+
+
+
+export default router
