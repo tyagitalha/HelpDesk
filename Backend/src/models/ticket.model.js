1f801f473714205df93ddc24e73804e985752f17
MOHD TALHA <talhamd0110@gmail.com>
2026-09-08 13:50:01 +0530

    feat: add ticket model with schema definition for support tickets

---
+++ Backend/src/models/ticket.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const ticketSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ["hardware", "software", "network", "account", "other"]
+        },
+        priority: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ["high", "medium", "low"]
+        },
+        status: {
+            type: String,
+            enum: ["open", "in-progress", "resolved", "closed"],
+            default: "open"
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        assignTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Ticket = mongoose.model("Ticket", ticketSchema)
