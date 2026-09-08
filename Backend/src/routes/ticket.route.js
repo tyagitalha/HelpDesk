fb20daf572d053474cd97f3800ae74f7cc233675
MOHD TALHA <talhamd0110@gmail.com>
2026-09-08 13:55:04 +0530

    feat: add ticket routes for creating, retrieving, updating, and deleting tickets

---
+++ Backend/src/routes/ticket.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createTicket, deleteTicket, getAllTicket, getOneTicket, getTicket, updateTicket } from "../controller/ticket.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+router.route("/createTicket").post(verifyJWT, createTicket)
+router.route("/getAllTicket").get(verifyJWT, getAllTicket)
+router.route("/getTicket").get(verifyJWT, getTicket)
+router.route("/update/:ticketId").patch(verifyJWT, updateTicket)
+router.route("/delete/:ticketId").delete(verifyJWT, deleteTicket)
+router.route("/getOneTicket/:ticketId").get(verifyJWT, getOneTicket)
+
+export default router
