efcda76b5fbdb67e18888a3b0ce93b29da9489d7
MOHD TALHA <talhamd0110@gmail.com>
2026-09-08 13:54:21 +0530

    feat: add admin middleware to restrict access to admin users

---
+++ Backend/src/middlewares/admin.middleware.js
@@ -0,0 +1,16 @@
+
+import { ApiError } from "../utils/apiError.js"
+
+export const isAdmin = async (req, res, next) => {
+ 
+    try {
+
+        if (req.user?.role !== "Admin") {
+            throw new ApiError(404, "Access Denied , Admin Only")
+        }
+
+        next()
+    } catch (error) {
+        throw new ApiError(500, "Something Went Wrong")
+    }
+}
