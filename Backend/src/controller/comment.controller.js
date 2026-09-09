1ee8410d51727636666f50415a679c1f0726ad7b
MOHD TALHA <talhamd0110@gmail.com>
2026-09-09 09:47:01 +0530

    fix: refactor createComment function for improved error handling and clarity

--- Backend/src/controller/comment.controller.js
+++ Backend/src/controller/comment.controller.js
@@ -2,37 +2,49 @@ import { ApiError } from "../utils/apiError.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/apiResponse.js"
 import { Comment } from "../models/comment.model.js"
+import { Ticket } from "../models/ticket.model.js";
 
 const createComment = asyncHandler(async (req, res) => {
+    const { message } = req.body;
+    const userId = req.user?._id;
+    const userRole = req.user?.role;
+    const { ticketId } = req.params;
 
-    const { message } = req.body
+    const ticket = await Ticket.findById(ticketId);
 
-    const userId = req.user?._id
-    const { ticketId } = req.params
-
-    const comment = await Comment.create(
-        {
-            message,
-            author: userId,
-            ticket: ticketId
-
-        }
-    )
-
-
-
-    console.log("comment", comment);
-
-    const createdComment = await Comment.findById(comment._id)
-
-    if (!createdComment) {
-        throw new ApiError(500, "Something went Wrong")
+    if (!ticket) {
+        throw new ApiError(404, "Ticket not found");
     }
 
-    return res.status(200)
-        .json(new ApiResponse(200, { Message: message }, "Comment create SuccessFull"))
+    // Normal user → sirf apne ticket par comment
+    // Admin → kisi bhi ticket par comment
+    if (
+        userRole !== "Admin" &&
+        ticket.createdBy.toString() !== userId.toString()
+    ) {
+        throw new ApiError(403, "You cannot comment on this ticket");
+    }
 
-})
+    const comment = await Comment.create({
+        message,
+        author: userId,
+        ticket: ticketId
+    });
+
+    const createdComment = await Comment.findById(comment._id);
+
+    if (!createdComment) {
+        throw new ApiError(500, "Something went wrong");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            { comment: createdComment },
+            "Comment created successfully"
+        )
+    );
+});
 
 const getAllComment = asyncHandler(async (req, res) => {
 
